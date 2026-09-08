07b11e90ae70f40a0319bb3c1f6c5eef20ab39bc
Extsup <ciklah7878@gmail.com>
2026-09-08 10:43:18 +0700

    Configure Compose compiler plugin for Kotlin 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android { namespace = "com.terra.gba"; compileSdk = 35
     defaultConfig { applicationId = "com.terra.gba"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "0.1.0" }
     buildFeatures { compose = true; buildConfig = true }
-    composeOptions { kotlinCompilerExtensionVersion = "1.5.15" }
     packaging { jniLibs { useLegacyPackaging = false } }
 }
 
